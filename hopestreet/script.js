bbadf8fee83c58edba1c639e94694cd1cdba3fe5
eamonmagree <110213420+eamonmagree@users.noreply.github.com>
2022-10-19 13:32:15 +1100

    Add files via upload

--- hopestreet/script.js
+++ hopestreet/script.js
@@ -1,3 +1,5 @@
+window.onload = function() {
+
 // first draggable div
 letdraggableDiv = document.getElementsByClassName("draggable")[0];
 
@@ -9,21 +11,23 @@ let dragStartX, dragStartY, dragObjLeft, dragObjTop;
 // button to do something
 
 // example for range
-window.addEventListener("mousemove", checkMousePosition);
+/* window.addEventListener("mousemove", checkMousePosition); */
 
-function checkMousePosition(e){
-    // console.log(e.clientX);
-    if(e.clientX > 422) {
+}
+
+/* function checkMousePosition(e){
+    console.log(e.clientX);
+    if(e.clientX > 422){
         conosle.log("right");
     } else {
         console.log("left");
     }  
-}
+} */
 
 // range 0 => 844
 
-/* screen.addEventListener("orientationchange", () => {
+screen.addEventListener("orientationchange", () => {
     console.log(`The orientation of the screen is: ${screen.orientation}`);
   });  
 
-screen.orientation.lock("landscape") ; */
+screen.orientation.lock("landscape") ;
